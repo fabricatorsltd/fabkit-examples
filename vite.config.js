bbef1a3a04ff7a80804511667a79075229a054ae
mirkobrombin <brombin94@gmail.com>
2026-02-18 16:30:37 +0000

    build: treat 'fabkit' as noExternal in SSR; remove alias override

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    resolve: {
-        alias: {
-            'fabkit': path.resolve('../src/lib')
-        }
-    },
-    server: {
-        fs: {
-            allow: ['..']
-        }
-    }
+  plugins: [sveltekit()],
+  ssr: {
+    noExternal: ['fabkit']
+  }
 });
